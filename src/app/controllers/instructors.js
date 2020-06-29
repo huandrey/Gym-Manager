02b42cc5954112928642d8383f3f2af777fbf6a8
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2020-06-29 15:10:44 -0300

    :rocket: Create instructors.js

---
+++ src/app/controllers/instructors.js
@@ -0,0 +1,51 @@
+const Instructor = require('../models/Instructor');
+
+module.exports = {
+    index: (req, res) => {
+        //all(callback) = all(function())
+        //callback(results.rows) = function(instructors)
+        //all(callback) = all(function(instructors));
+        Instructor.all(function(instructors) {
+            return res.render('instructors/index', { instructors })
+        })
+    },
+    create: (req, res) => {
+        return
+    },
+    show: (req, res) => {
+        return
+    },
+    post: (req, res) => {
+        // Verificando se todos os campos do formulário estão preenchidos
+        const keys = Object.keys(req.body);
+        
+        for (key of keys) {
+            if(req.body[key] == "") res.send('please, ')
+        }
+        Instructor.create(req.body, function(instructor) {
+            return res.redirect('/');
+        })
+
+        // Instructor.create(req.body, function(instructor) {
+        //     return res.redirect(`instructors/${instructor.id}`);
+        // })
+
+    },
+
+    edit: (req, res) => {
+        return
+    },
+
+    put: (req, res) => {
+        const keys = Object.keys(req.body);
+        
+        for (key of keys) {
+            if(req.body[key] === "") res.send('please, ')
+        }
+        return
+    },
+
+    delete: (req, res) =>{
+     return   
+    }
+}
